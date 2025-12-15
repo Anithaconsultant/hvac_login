eb9dbdb766fc89dab6be90be98e038f48ad1927c
Anithaconsultant <anitha@iihs.ac.in>
2025-12-15 19:02:03 +0530

    removed backup

--- phantomload_dev - Copy/accounts/utils.py
+++
@@ -1,7 +0,0 @@
-
-import string
-import random
-
-def generate_short_id(length=8):
-    characters = string.ascii_letters + string.digits  # a-zA-Z0-9
-    return ''.join(random.choices(characters, k=length))
