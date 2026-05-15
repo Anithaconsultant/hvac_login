6525863796f5629e49c75e2d9b5f870e5628651c
Anitha <anitha@iihs.ac.in>
2026-05-15 11:50:17 +0530

    bug:id changes in  model

--- phantomload_dev/accounts/models.py
+++ phantomload_dev/accounts/models.py
@@ -57,7 +57,7 @@ def generate_group_id():
 class CustomUser(AbstractUser):
     username = None
 
-    user_id = models.CharField(
+    id = models.CharField(
         primary_key=True,
         max_length=8,
         unique=True,
